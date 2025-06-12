c2b5ae159a42442ef6e2561a98144c480ce31da1
Saurabhkumarkushwaha <sk62054847@gmail.com>
2025-06-12 10:05:12 +0530

    Code added

---
+++ README.md
@@ -0,0 +1,23 @@
+# Requirements
+
+-User should be able to create a post
+  -[The post/Tweet cannot be more than 250 chars]
+  -[Every post/tweet will be having support for image upload]
+
+-Any post should be visible to all the users who follows you.
+-Anyone who follows you can comment on a post/tweet
+-Anyone who follows you can like on a post/tweet
+-We can comment on a comment
+-we can like any comment also
+-Retwitting
+
+-User Profile
+  -Name
+  -Follower Count
+  -Bio
+  -Last 10 tweets from the user
+
+-pagination on tweets
+-User Auth
+
+-Every Tweet might be having a hashtag
--- src/index.js
+++ src/index.js
@@ -9,22 +9,4 @@ app.listen(3000, async() => {
     console.log("Server Started");
     await connect();  
     console.log('Mongo db connected');
-    // const tweet = await Tweet.create({
-    //     content: 'First tweet',
-    //     userEmail: 'a@b.com'
-    // });
-    //const tweets = await Tweet.find({userEmail: 'a@b.com'}); 
-    // const tweet = await Tweet.findById('3445255tgth644hh56');
-    // tweet.userEmail = 'b@c.com';
-    // await tweet.save();
-    const tweetRepo = new TweetRepository();
-    // const tweet = await tweetRepo.create({content: 'Tweet with comment schema'});
-    // console.log(tweet);
-    // const comment = await Comment.create({content: 'new Comment'});
-    // tweet.comments.push(comment); 
-    // await tweet.save();
-    // const tweet = await tweetRepo.getAll(2, 4); 
-    // console.log(tweet[0].contentWithEmail);
-    const tweet = await tweetRepo.create({content: 'With hooks'});
-    console.log(tweet);
 }); 
