bf94e4559091ab596d98fcee647b1354bb8e40cc
Saurabhkumarkushwaha <sk62054847@gmail.com>
2025-06-13 16:42:12 +0530

    Toggle like working

--- src/index.js
+++ src/index.js
@@ -5,18 +5,25 @@ import {connect} from './config/database.js';
 
 import apiRoutes from './routes/index.js';
 
+import { UserRespository, TweetRepository } from './repository/index.js';
+import LikeService from './services/like-service.js';
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-import service from './services/tweet-service.js';
 
 app.listen(3000, async() => {
     console.log("Server Started");
     await connect();  
     console.log('Mongo db connected');
-    let ser = new service();
-    await ser.create({content: 'l do #CoDes to #works and gain #Knowledge?'})
+
+    const userRepo = new UserRespository();
+    const tweetRepo = new TweetRepository();
+    const tweets = await tweetRepo.getAll(0, 10);
+    const users = await userRepo.getAll();
+    const likeService = new LikeService();
+    await likeService.toggleLike(tweets[0].id, 'Tweet', users.id);
 });  
---
+++ src/services/like-service.js
@@ -0,0 +1,47 @@
+import {LikeRespository, TweetRepository} from "../repository/index.js";
+import Tweet from '../models/tweet.js';
+
+class LikeService {
+    constructor() {
+        this.likeRepository = new LikeRespository();
+        this.tweetRepository = new TweetRepository();
+    }
+
+    async toggleLike(modelId, modelType, userId) {// /api/v1/likes/toggle?id=modelid&type=Tweet
+        console.log(modelId);
+        if(modelType == 'Tweet') {
+            var likeable = await this.tweetRepository.find(modelId);
+            
+        }else if(modelType == 'Comment') {
+            //TODO
+        }else{
+            throw new Error('unknown model type');
+        }
+        console.log("Beyond");
+        const exists = await this.likeRepository.findByUserAndLikable({
+            user: userId,
+            onModel: modelType,
+            likeable: modelId
+        });
+        if(exists) {
+            likeable.likes.pull(exists.id);
+            await likeable.save();
+            await exists.remove();
+            var isAdded = false;
+
+        }else {
+            const newLike = await this.likeRepository.create({
+                user:userId,
+                onModel: modelType,
+                likeable: modelId
+            });
+            likeable.likes.push(newLike);
+            await likeable.save();
+
+            var isAdded = true;
+        }
+        return isAdded;
+    }
+}
+
+export default LikeService;
