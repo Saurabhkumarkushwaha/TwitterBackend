78209218ae061e298c59893d88bcdee0b456054c
Saurabhkumarkushwaha <sk62054847@gmail.com>
2025-06-19 14:25:32 +0530

    Added passport logic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import passport from 'passport';
 
 import {connect} from './config/database.js';
 
+import {passportAuth} from './config/jwt-middleware.js';
+
 import apiRoutes from './routes/index.js';
 
-import { UserRespository, TweetRepository } from './repository/index.js';
-import LikeService from './services/like-service.js';
-//import service from './services/tweet-service.js';
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(passport.initialize());
+passportAuth(passport);
 
 app.use('/api', apiRoutes);
 
@@ -19,5 +22,4 @@ app.listen(3000, async() => {
     console.log("Server Started");
     await connect();  
     console.log('Mongo db connected');
-
 });  
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -16,7 +18,6 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true});
 
-
 userSchema.pre('save', function(next) {
     const user = this;
     const SALT = bcrypt.genSaltSync(9);
@@ -24,6 +25,16 @@ userSchema.pre('save', function(next) {
     user.password = encryptedPassword;
     next();
 });
+
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+}
+
+userSchema.methods.genJWT = function generate() {
+    return jwt.sign({id: this._id, email: this.email},'twitter_secret', {
+        expiresIn: '1h'
+    });
+}
 
 const User = mongoose.model('User', userSchema);
 
